96d3bab19aa3b099fd510797a0fb06ebc3eef826
Melwin Mendonca <melwinmanish30@gmail.com>
2026-07-01 17:36:10 +0530

    initial commit

---
+++ src/main/java/com/example/assetmanagementgateway/AssetManagementGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.assetmanagementgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManagementGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetManagementGatewayApplication.class, args);
+    }
+
+}
